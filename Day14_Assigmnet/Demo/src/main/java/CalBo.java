873bab3dd1a9325a288725b10ae9d0ddf5f9586c
Divyesh Variya <Divyesh.Variya@einfochips.com>
2023-02-24 12:28:03 +0530

    commit on 24-01-2023 work

---
+++ Day14_Assigmnet/Demo/src/main/java/CalBo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
+public class CalBo {
+	
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+
+	public ArrayList<Integer> getAll()
+	{
+		ArrayList<Integer> li= new ArrayList<Integer>();
+		li.add(10);
+		li.add(21);
+		return li;
+	}
+}
